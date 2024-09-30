da97c2f90b1203cd297c93a277016e73fbed3d79
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-09-30 08:46:09 -0300

    feat: add gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-black flex justify-center items-center pointer-events-none select-none bg-sky-pattern">
+    <div className="w-full h-screen bg-black flex flex-col justify-center items-center pointer-events-none select-none bg-sky-pattern">
       <img
         className="max-w-[340px] w-full"
         src="/wizard.gif"
+      />
+      <img
+        className="max-w-[440px] w-full mt-4"
+        src="/stuff.gif"
       />
     </div>
   );
