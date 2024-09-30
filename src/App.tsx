e841dd1216415969985a416c6b3c79b75c750d0a
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-09-30 08:51:18 -0300

    feat: add weed gif

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 function App() {
   return (
-    <div className="w-full h-screen bg-black flex flex-col justify-center items-center pointer-events-none select-none bg-sky-pattern">
+    <div className="w-full h-screen bg-black flex flex-col gap-4 justify-center items-center pointer-events-none select-none bg-sky-pattern">
+      <img
+        className="max-w-[80px] w-full"
+        src="/weed.gif"
+      />
       <img
         className="max-w-[340px] w-full"
         src="/wizard.gif"
       />
       <img
-        className="max-w-[440px] w-full mt-4"
+        className="max-w-[440px] w-full"
         src="/stuff.gif"
       />
     </div>
